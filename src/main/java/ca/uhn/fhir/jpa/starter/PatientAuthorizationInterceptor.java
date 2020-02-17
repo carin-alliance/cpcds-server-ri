1307dc165895b28f4535344bd76462cb88243060
Benjamin Langley <blangley@mitre.org>
2020-02-17 14:07:05 -0500

    Resource references fix

--- src/main/java/ca/uhn/fhir/jpa/starter/PatientAuthorizationInterceptor.java
+++ src/main/java/ca/uhn/fhir/jpa/starter/PatientAuthorizationInterceptor.java
@@ -20,13 +20,13 @@ public class PatientAuthorizationInterceptor extends AuthorizationInterceptor {
         boolean userIsAdmin = false;
         String authHeader = theRequestDetails.getHeader("Authorization");
         if ("Bearer dfw98h38r".equals(authHeader)) {
-            // This user has access only to Patient/465cd4ab-4b71-4765-963b-c90fb19257be
+            // This user has access only to Patient/1
             // resources
-            userIdPatientId = new IdType("Patient", "465cd4ab-4b71-4765-963b-c90fb19257be");
+            userIdPatientId = new IdType("Patient", "1");
         } else if ("Bearer 89gh653a2".equals(authHeader)) {
-            // This user has access only to Patient/02d45714-445e-4093-aec5-5873f17c9ba8
+            // This user has access only to Patient/689
             // resources
-            userIdPatientId = new IdType("Patient", "02d45714-445e-4093-aec5-5873f17c9ba8");
+            userIdPatientId = new IdType("Patient", "689");
         } else if ("Bearer 39ff939jgg".equals(authHeader)) {
             // This user has access to everything
             userIsAdmin = true;
