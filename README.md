013fc8380cf73fdc5c0b2038ce9e3f3e3d29c509
Benjamin Langley <blangley@mitre.org>
2020-02-18 15:10:56 -0500

    Implement JWT tokens for patient authorization

--- README.md
+++ README.md
@@ -43,6 +43,16 @@ The server will then be browesable at http://localhost:8080/cpcds-server and the
 
 Note: This has only been tested using Java 8. Later version may not be supported.
 
+## GET Requests
+
+The server uses JWT tokens to authenticate the user on the server. At this point the payload is insecure (will be changing). The tokens are signed using HMAC SHA256 with "secret" as the secret. For production versions the secret should be 64 bits and obviously kept secret. The server expects the `iss` claim to be `cpcds-server-ri`. The `patient.id` is encoded in the `patient` claim as a string. The JWT tokens for a few test patients is provided below.
+
+| Patient ID | JWT token                                                                                                                                                         |
+| ---------- | :---------------------------------------------------------------------------------------------------------------------------------------------------------------- |
+| 1          | eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJjcGNkcy1zZXJ2ZXItcmkiLCJwYXRpZW50IjoiMSIsImlhdCI6MTU4MjA1NDEzNX0.soTbe6tuu0pkNUkmLZJ24dLH9KvSunLkWxul07mV4a0      |
+| 689        | eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJjcGNkcy1zZXJ2ZXItcmkiLCJwYXRpZW50IjoiNjg5IiwiaWF0IjoxNTgyMDU0MTM1fQ.6Xgy_d5MBi316cSEmqmJJY_s065uClmcjnNePnQkZuk   |
+| admin      | eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJjcGNkcy1zZXJ2ZXItcmkiLCJwYXRpZW50IjoiYWRtaW4iLCJpYXQiOjE1ODIwNTQxMzV9.nUZKr9WUMXPG2v3iDSbz03fcZJm41nUHIiNmL80PJh0 |
+
 ## Uploading Test Data
 
 The Ruby script `upload.rb` uploads test data into the FHIR server. The easiest way to run is with [Bundler](https://bundler.io/). To install Bundler
--- src/main/java/ca/uhn/fhir/jpa/starter/PatientAuthorizationInterceptor.java
+++ src/main/java/ca/uhn/fhir/jpa/starter/PatientAuthorizationInterceptor.java
@@ -1,6 +1,14 @@
 package ca.uhn.fhir.jpa.starter;
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 
 import org.hl7.fhir.r4.model.IdType;
 
@@ -18,21 +26,33 @@ public class PatientAuthorizationInterceptor extends AuthorizationInterceptor {
 
         IdType userIdPatientId = null;
         boolean userIsAdmin = false;
+
+        // Get the JWT token from the Authorization header
         String authHeader = theRequestDetails.getHeader("Authorization");
-        if ("Bearer dfw98h38r".equals(authHeader)) {
-            // This user has access only to Patient/1
-            // resources
-            userIdPatientId = new IdType("Patient", "1");
-        } else if ("Bearer 89gh653a2".equals(authHeader)) {
-            // This user has access only to Patient/689
-            // resources
-            userIdPatientId = new IdType("Patient", "689");
-        } else if ("Bearer 39ff939jgg".equals(authHeader)) {
-            // This user has access to everything
-            userIsAdmin = true;
-        } else {
-            // Throw an HTTP 401
-            throw new AuthenticationException("Missing or invalid Authorization header value");
+        String regex = "Bearer (.*)";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(authHeader);
+        String token = "";
+        if (matcher.find() && matcher.groupCount() == 1)
+            token = matcher.group(1);
+        else
+            throw new AuthenticationException("Authorization header is not in the form \"Bearer <token>\"");
+
+        try {
+            // Verify and decode the JWT token
+            Algorithm algorithm = Algorithm.HMAC256("secret");
+            JWTVerifier verifier = JWT.require(algorithm).withIssuer("cpcds-server-ri").build();
+            DecodedJWT jwt = verifier.verify(token);
+            String patientId = jwt.getClaim("patient").asString();
+
+            // Set the userIdPatientId based on the token
+            if (patientId.equals("admin"))
+                userIsAdmin = true;
+            else
+                userIdPatientId = new IdType("Patient", patientId);
+        } catch (JWTVerificationException exception) {
+            // Invalid signature/claims
+            throw new AuthenticationException("Authorization failed: bad signature");
         }
 
         // If the user is a specific patient, we create the following rule chain:
