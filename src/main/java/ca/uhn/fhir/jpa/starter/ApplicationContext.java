c32f895f9f776e60997b639e18687866366584ff
Benjamin Langley <blangley@mitre.org>
2020-11-10 17:48:09 -0500

    Remove unused files

--- src/main/java/ca/uhn/fhir/jpa/starter/ApplicationContext.java
+++ src/main/java/ca/uhn/fhir/jpa/starter/ApplicationContext.java
@@ -7,11 +7,7 @@ public class ApplicationContext extends AnnotationConfigWebApplicationContext {
 
     public ApplicationContext() {
         FhirVersionEnum fhirVersion = HapiProperties.getFhirVersion();
-        if (fhirVersion == FhirVersionEnum.DSTU2) {
-            register(FhirServerConfigDstu2.class, FhirServerConfigCommon.class);
-        } else if (fhirVersion == FhirVersionEnum.DSTU3) {
-            register(FhirServerConfigDstu3.class, FhirServerConfigCommon.class);
-        } else if (fhirVersion == FhirVersionEnum.R4) {
+        if (fhirVersion == FhirVersionEnum.R4) {
             register(FhirServerConfigR4.class, FhirServerConfigCommon.class);
         } else if (fhirVersion == FhirVersionEnum.R5) {
             register(FhirServerConfigR5.class, FhirServerConfigCommon.class);
