e93d7a326a157abc1d357f781a6d2e56927e3d21
Benjamin Langley <blangley@mitre.org>
2021-04-14 15:45:53 -0400

    Admin token bypass readonly interceptor

--- src/main/java/ca/uhn/fhir/jpa/starter/ReadOnlyInterceptor.java
+++ src/main/java/ca/uhn/fhir/jpa/starter/ReadOnlyInterceptor.java
@@ -14,9 +14,9 @@ public class ReadOnlyInterceptor extends InterceptorAdapter {
     public void incomingRequestPreHandled(RestOperationTypeEnum theOperation,
             ActionRequestDetails theProcessedRequest) {
         String authHeader = theProcessedRequest.getRequestDetails().getHeader("Authorization");
-        String adminToken = System.getenv("ADMIN_TOKEN");
-        if (!authHeader.equals("Bearer " + adminToken)
-                && theOperation != RestOperationTypeEnum.HISTORY_INSTANCE
+        String adminHeader = "Bearer " + System.getenv("ADMIN_TOKEN");
+        if (adminHeader.equals(authHeader)) return;
+        else if (theOperation != RestOperationTypeEnum.HISTORY_INSTANCE
                 && theOperation != RestOperationTypeEnum.HISTORY_SYSTEM
                 && theOperation != RestOperationTypeEnum.HISTORY_TYPE && theOperation != RestOperationTypeEnum.METADATA
                 && theOperation != RestOperationTypeEnum.READ && theOperation != RestOperationTypeEnum.SEARCH_SYSTEM
