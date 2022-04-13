c3884be709cb5dd1dccae136b0868f2dae8135b0
Vanessa Fotso <vfotso@mitre.org>
2022-04-13 10:20:08 -0400

    Untracked files

--- initiate-upload.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-rm -rf ./data
-wait
-docker build -t cpcds-write .
-docker compose up
--- seed-db-process.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-bundle install
-bundle exec ruby upload.rb
-# wait
-# curl http://localhost:8080/cpcds-server/fhir/$mark-all-resources-for-reindexing?_format=json
-# curl http://localhost:8080/cpcds-server/fhir/$perform-reindexing-pass?_format=json
